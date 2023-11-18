7eb2bf36d792a1cf669eda30fd41ed0be679ed51
Trong Hai <truongtronghai@gmail.com>
2023-11-18 13:31:00 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <NuxtLink to="/" class="font-semibold text-gray-50 hover:text-gray-400 duration-100"> Home </NuxtLink>
+        <NuxtWelcome />
+    </div>
+</template>
