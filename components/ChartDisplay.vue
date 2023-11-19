be35df3723aa957b25e2785c31cb421e9a2cb19e
Trong Hai <truongtronghai@gmail.com>
2023-11-19 11:22:49 +0700

    add VueChartJs and sample

---
+++ components/ChartDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+    <Doughnut id="chartjs-4" class="chartjs" :data="data" :options="chartOptions" />
+</template>
+<script setup lang="ts">
+import { Doughnut } from 'vue-chartjs'
+import { Chart, ArcElement } from 'chart.js'
+
+Chart.register(ArcElement)
+
+// const props = defineProps<{
+//     type: string
+// }>()
+const data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+        {
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+            hoverOffset: 4,
+        },
+    ],
+}
+const chartOptions = { responsive: true }
+</script>
