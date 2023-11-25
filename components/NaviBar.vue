c50006d8414510e990912ee78aef4b7abd9809e8
Trong Hai <truongtronghai@gmail.com>
2023-11-25 20:43:06 +0700

    enable left drawer

--- components/NaviBar.vue
+++ components/NaviBar.vue
@@ -1,8 +1,19 @@
 <template>
-    <v-navigation-drawer>
-        <v-list>
-            <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-    </v-navigation-drawer>
+    <client-only>
+        <v-navigation-drawer :rail="rail" @click.stop="rail = false">
+            <v-list>
+                <v-list-item title="Home">
+                    <template #prepend>
+                        <v-icon icon="mdi-home-city" color="pink-accent-4" />
+                    </template>
+                    <template #append>
+                        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+                    </template>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </client-only>
 </template>
-<script setup></script>
+<script setup>
+const rail = ref(true)
+</script>
