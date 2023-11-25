8b5fe69e3c63f23630f8a56bea51da313587f04c
Trong Hai <truongtronghai@gmail.com>
2023-11-25 13:46:25 +0700

    add theme change button

--- components/NaviBar.vue
+++ components/NaviBar.vue
@@ -1,8 +1,12 @@
 <template>
-    <nav id="header" class="bg-white fixed w-full z-10 top-0 shadow">
+    <nav id="header" class="fixed w-full z-10 top-0 shadow px-2" :class="{ 'bg-black': isDark, 'bg-white': !isDark }">
         <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
             <div class="w-1/2 pl-2 md:pl-0">
-                <a class="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
+                <a
+                    class="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold"
+                    href="#"
+                    @click="handleThemeClick"
+                >
                     <i class="fas fa-sun text-pink-600 pr-3"></i> Admin Day Mode
                 </a>
             </div>
@@ -79,8 +83,8 @@
             </div>
 
             <div
-                class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white z-20"
                 id="nav-content"
+                class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white z-20"
             >
                 <ul class="list-reset lg:flex flex-1 items-center px-4 md:px-0">
                     <li class="mr-6 my-2 md:my-0">
@@ -149,3 +153,9 @@
         </div>
     </nav>
 </template>
+<script setup>
+const isDark = ref(false)
+const handleThemeClick = () => {
+    isDark.value = !isDark.value
+}
+</script>
