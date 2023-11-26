16a92c73991d71c5ae7c7e31047220f916acd124
Trong Hai <truongtronghai@gmail.com>
2023-11-26 11:18:33 +0700

    add mobile responsive for drawer

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,21 @@
 <template>
-    <v-app-bar class="px-2 d-flex justify-space-between">
-        <div class="d-flex me-auto">
+    <v-app-bar class="pa-2 d-flex justify-space-between">
+        <div class="d-flex">
             <v-icon icon="mdi-finance" color="green" class="align-self-center" />
-            <h2 class="text-h2 ml-2">Coinmate</h2>
+            <h2 class="text-h2 ml-2 hidden-xs">Coinmate</h2>
+            <h2 class="text-h4 ml-2 d-xs hidden-sm-and-up">Coinmate</h2>
         </div>
         <v-btn icon="mdi-white-balance-sunny" @click="handleClickChangeTheme"></v-btn>
+        <v-spacer />
+        <v-app-bar-nav-icon variant="text" class="hidden-sm-and-up" @click="handleSwitchMobDrawer"></v-app-bar-nav-icon>
     </v-app-bar>
 </template>
 <script setup>
-const emit = defineEmits(['changeTheme'])
+const emit = defineEmits(['changeTheme', 'switchMobDrawer'])
 const handleClickChangeTheme = () => {
     emit('changeTheme')
+}
+const handleSwitchMobDrawer = () => {
+    emit('switchMobDrawer')
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,20 @@
 <template>
     <v-app>
         <v-theme-provider :theme="isDark ? 'dark' : 'light'" with-background>
-            <Header @change-theme="handleChangeTheme" />
+            <Header @change-theme="handleChangeTheme" @switch-mob-drawer="handleMobDrawer" />
             <NaviBar />
+            <NaviBarMobile :show="isMobile" />
             <NuxtPage />
         </v-theme-provider>
     </v-app>
 </template>
 <script setup>
 const isDark = ref(false)
+const isMobile = ref(false)
 const handleChangeTheme = () => {
     isDark.value = !isDark.value
+}
+const handleMobDrawer = () => {
+    isMobile.value = !isMobile.value
 }
 </script>
